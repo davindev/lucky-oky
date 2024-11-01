33e758109e6ac92d46409a22bc16efd87108c809
davindev <vinnie.jeong@kidsnote.com>
2024-11-01 16:09:53 +0900

    feat: 메인 페이지 수정

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,41 @@
-import { Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+import { useBatteryLevel } from 'expo-battery';
+
+const MAX_BATTERY_LEVEL = 5;
 
 export default function HomeScreen() {
-  return <Text style={styles.text}>123</Text>
+  const [nickname, setNickname] = useState(''); // TODO 전역 상태로 관리
+
+  const initialBatteryLevel = useBatteryLevel();
+  const batteryLevel = +initialBatteryLevel.toFixed(2) * 100;
+
+  if (batteryLevel > MAX_BATTERY_LEVEL) {
+    return (
+      <View style={styles.container}>
+        <Text>배터리가 {MAX_BATTERY_LEVEL}% 이하일 때 다시 와줘!</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>배터리가 {batteryLevel}% 남았다니 완전 럭키 비키잖아~</Text>
+        <TextInput value={nickname} placeholder="닉네임을 입력해!" onChangeText={setNickname} />
+        <Link href="/chat">가보자고</Link>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    paddingTop: 100,
-    color: 'red',
+  container: {
+    width: 100,
+    height: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
